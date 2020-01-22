c5a323b4d4c6d11b77adccbf8f15bde5d81b4a9d
Nina Unger <60093065+ninaunger@users.noreply.github.com>
2020-01-22 14:05:27 +0200

    exercise2
    the change in style.css file is at the end of the file (#additional-footer)

--- pubPlus.php
+++ pubPlus.php
@@ -5,14 +5,16 @@ Description: Nina Unger - Test
 Version: 1.0
 Author: Nina Unger
 */
-function get_query_param() {
-   
-      echo '<script type="text/javascript" src="' . get_theme_file_uri() . '/assets/js/getQueryStringParameter.js"></script>';
+
+
+function insert_footer()
+{
+    echo '<div id ="additional-footer" >Additional Footer</div>';
+
 }
 
-// Add hook in wp_head <head></head>
-add_action( 'wp_head', 'get_query_param' );
-add_action( 'wp_enqueue_script', 'get_query_param' );
+add_action( 'wp_footer', 'insert_footer' );
+
 
 
 ?>
